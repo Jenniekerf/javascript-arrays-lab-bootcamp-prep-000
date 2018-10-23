ce9493a4b5c6501603d559f1821f9ff5af3c89ec
Jennie Kerfstedt <jenniekerf@gmail.com>
2018-10-23 22:36:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var kittens = ['Milo', 'Otis', 'Garfield' ];
-var newCat = 'Ralph';
+
 //define your array here
 // Add your functions and code here
 
-function destructivelyAppendKitten(kittens, newCat) {
-  kittens.push(newCat);
+function destructivelyAppendKitten(kittens, 'Ralph') {
+  kittens.push('Ralph');
   return kittens;
 }
